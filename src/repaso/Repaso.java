30079a0802e6c3e4e26552c7ebefdb45318f2031
Usuario DAM 1 <Usuario DAM 1@DESKTOP-R6MG806>
2019-10-03 10:05:03 +0200

    Merge origin/master

---
+++ src/repaso/Repaso.java
@@ -0,0 +1,14 @@
+package repaso;
+/**
+ *
+ * @author Diego_Eloy DAM 1
+ */
+public class Repaso {
+   
+    public static void main(String[] args) {
+        
+        int numero1=10;
+        int numero2=10;
+        
+    }
+}
